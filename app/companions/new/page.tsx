316a54c724a7e8c03617a7bfd786fad173f93a3f
JOSHUOFFI <chi2003david@gmail.com>
2026-06-25 10:24:26 -0700

    first commit

---
+++ app/companions/new/page.tsx
@@ -0,0 +1,11 @@
+
+
+const NewCompanion = () => {
+  return (
+    <div>
+      New Companion
+    </div>
+  )
+}
+
+export default NewCompanion
---
+++ app/companions/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CompanionsLibrary = () => {
+  return (
+    <div>
+      Companion Library
+    </div>
+  )
+}
+
+export default CompanionsLibrary
